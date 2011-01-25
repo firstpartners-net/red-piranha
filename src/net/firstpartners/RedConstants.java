f17b92a5c44198f14b8415e63499933b5537c52d
paul.m.browne <paul.m.browne@36e6750b-ea9a-a97a-8f16-d4ebf86e793c>
2011-01-25 22:21:34 +0000

    RP Version 4 - Params can be set by servlet

---
+++ src/net/firstpartners/RedConstants.java
@@ -0,0 +1,15 @@
+package net.firstpartners;
+
+/**
+ * Place holder for Constants
+ * @author paul
+ *
+ */
+public class RedConstants{
+
+	// the name of the sheet the we log files to
+	public static final String EXCEL_LOG_WORKSHEET_NAME = "log";
+
+	public static final String XLS_FILE_EXTENSION = ".xls";
+
+}

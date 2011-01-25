a594bcbdc7abfc5e2b71c21325908bcdf4a0b48f
paul.m.browne <paul.m.browne@36e6750b-ea9a-a97a-8f16-d4ebf86e793c>
2011-01-25 22:16:01 +0000

    RP Version 4 - Params can be set by servlet

--- test/net/firstpartners/drools/URLRuleLoaderTest.java
+++ test/net/firstpartners/drools/URLRuleLoaderTest.java
@@ -1,6 +1,7 @@
 package net.firstpartners.drools;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
 import java.io.IOException;
 import java.util.logging.Logger;
@@ -19,7 +20,6 @@ public class URLRuleLoaderTest {
 			.getName());
 
 
-
 	@Test
 	public final void testLoadCachedRulesFromUrl() throws IOException, ClassNotFoundException {
 
@@ -33,8 +33,44 @@ public class URLRuleLoaderTest {
 
 		//Check that we can do things with the retrieved knowledgebase
 		assertNotNull(kb);
-
 		StatefulKnowledgeSession sks = kb.newStatefulKnowledgeSession();
 	}
+
+	@Test
+	public final void testSecurityManagerPrefixOnUrlLoad() throws IOException, ClassNotFoundException{
+
+		//check that dodgy domains are caught
+		//Set the rule source
+		RuleSource ruleSource = new RuleSource();
+		ruleSource.setKnowledgeBaseLocation("http://www.bbc.co.uk/not-on-whitelist-should-fail.Knowledgebase");
+
+		//Check that we cannot read rules from remote location
+		try{
+			KnowledgeBase kb = ruleLoader.loadKnowledgeBase(ruleSource);
+			fail("Expected Security Exception not thrown");
+
+		} catch (SecurityException se){
+			// do nothing - we need the security exception to be thrown for the test to pass
+		}
+	}
+
+	@Test
+	public final void testSecurityManagerSuffixOnUrlLoad() throws IOException, ClassNotFoundException{
+
+		//check that dodgy domains are caught
+		//Set the rule source
+		RuleSource ruleSource = new RuleSource();
+		ruleSource.setKnowledgeBaseLocation("http://red-piranha.appspot.com/sampleresources/SpreadSheetServlet/log-then-modify-rules.SomeUnknownResourceType");
+
+		//Check that we cannoread rules from remote location
+		try{
+			KnowledgeBase kb = ruleLoader.loadKnowledgeBase(ruleSource);
+			fail("Expected Security Exception not thrown");
+
+		} catch (SecurityException se){
+			// do nothing - we need the security exception to be thrown for the test to pass
+		}
+	}
+
 
 }

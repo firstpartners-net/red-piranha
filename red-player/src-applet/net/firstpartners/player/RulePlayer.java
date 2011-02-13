0d2572fafd0906b69062028386bcf76a4a825bbd
paul.m.browne <paul.m.browne@36e6750b-ea9a-a97a-8f16-d4ebf86e793c>
2011-02-13 22:12:46 +0000

    Draft docs for RulePlayer build

--- red-player/src-applet/net/firstpartners/player/RulePlayer.java
+++ red-player/src-applet/net/firstpartners/player/RulePlayer.java
@@ -1,140 +1,150 @@
 package net.firstpartners.player;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Graphics;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.io.IOException;
 
-import javax.swing.JApplet;
-import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import javax.swing.SwingUtilities;
 
 import net.firstpartners.drools.PreCompileRuleBuilder;
 
 import org.drools.compiler.DroolsParserException;
 
 /**
- * Applet to allow compilition (outside of GAE) of rules
+ * Java client to allow compilition (outside of GAE) of rules
+ * Some console code from RJHM van den Bergh , rvdb@comweb.nl
+ * 
  * 
  * @author paul
  * 
  */
-public class RulePlayer extends JApplet implements ActionListener {
 
-	// The component that actually presents the GUI.
-	public class Gui extends JPanel {
-		public Gui() {
-			super(new BorderLayout());
-		}
+public class RulePlayer extends WindowAdapter implements WindowListener,
+		ActionListener, Runnable {
 
-		protected void paintComponent(Graphics g) {
-			super.paintComponent(g);
+	
+	private JFrame frame;
+	private JTextArea textArea;
+	
+	private boolean quit =false;
 
-		}
-	}
+	void compileRules() {
 
-	Gui gui; // the applet's content pane
-	JTextArea mainTextArea;
-	String ruleFile = null;
+		String ruleFile = "http://red-piranha.googlecode.com/svn/trunk/war/sampleresources/SpreadSheetServlet/log-then-modify-rules.drl";
+		
+		
+		if (ruleFile == null) {
+			textArea.append("Please set 'rule' as a param pointing at the rule file you wish to compile ");
 
-	public void actionPerformed(ActionEvent e) {
-	}
+		} else {
+			textArea.append("Compiling Rules...\n");
+			textArea.append("Using file:" + ruleFile + "\n");
 
-	public String getAppletInfo() {
-		return "Title: Red Piranha Applet\n" + "Author: Paul Browne\n"
-				+ "Applet to package up rules";
-	}
-
-	public String[][] getParameterInfo() {
-		String[][] info = { { "rule", "string",
-				"the rule file we wish to compile" } };
-		return info;
-	}
-
-	// Called when this applet is loaded into the browser.
-	public void init() {
-		loadAppletParameters();
-
-		// Execute a job on the event-dispatching thread:
-		try {
-			SwingUtilities.invokeAndWait(new Runnable() {
-				public void run() {
-					setupGui();
-
-					if (ruleFile == null) {
-						mainTextArea.append("Please set 'rule' as a param pointing at the rule file you wish to compile ");
-						
-						
-					} else {
-						mainTextArea.append("Compiling Rules...\n");
-						mainTextArea.append("Using file:"+ruleFile+"\n");
-						
-						PreCompileRuleBuilder rulebuilder = new PreCompileRuleBuilder();
-						try {
-							rulebuilder.cacheRule(ruleFile, null);
-						} catch (DroolsParserException e) {
-							mainTextArea.append("DroolsParserException:" + e);
-							e.printStackTrace();
-						} catch (IOException e) {
-							mainTextArea
-									.append("IOException:" + e.getMessage());
-							e.printStackTrace();
-						} catch (ClassNotFoundException e) {
-							mainTextArea.append("ClassNotFoundException:"
-									+ e.getMessage());
-							e.printStackTrace();
-						}
-						mainTextArea.append("Compiling complete");
-					}
-				}
-			});
-
-		} catch (Throwable t) {
-			mainTextArea.append("Error:" + t);
-			t.printStackTrace();
+			PreCompileRuleBuilder rulebuilder = new PreCompileRuleBuilder();
+			try {
+				rulebuilder.cacheRule(ruleFile, null);
+			} catch (DroolsParserException e) {
+				textArea.append("DroolsParserException:" + e+"\n");
+				e.printStackTrace();
+			} catch (IOException e) {
+				textArea.append("IOException:" + e.getMessage()+"\n");
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				textArea.append("ClassNotFoundException:" + e.getMessage()+"\n");
+				e.printStackTrace();
+			}
+			textArea.append("Compiling complete\n");
 		}
 
 	}
 
-	// Called by init.
-	protected void loadAppletParameters() {
-		// Get the applet parameters.
-		String at = getParameter("rule");
-		if (at != null) {
-			ruleFile = at;
-		}
+	public RulePlayer() {
+		// create all components and add them
+		frame = new JFrame("Java Console");
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		Dimension frameSize = new Dimension((int) (screenSize.width / 2),
+				(int) (screenSize.height / 2));
+		int x = (int) (frameSize.width / 2);
+		int y = (int) (frameSize.height / 2);
+		frame.setBounds(x, y, frameSize.width, frameSize.height);
+
+		textArea = new JTextArea();
+		textArea.setEditable(false);
+		JButton button = new JButton("clear");
+
+		frame.getContentPane().setLayout(new BorderLayout());
+		frame.getContentPane().add(new JScrollPane(textArea),
+				BorderLayout.CENTER);
+		frame.getContentPane().add(button, BorderLayout.SOUTH);
+		frame.setVisible(true);
+
+		frame.addWindowListener(this);
+		button.addActionListener(this);
+
+	
+		quit = false; // signals the Threads that they should exit
+
 
 	}
 
 	/**
-	 * Create the GUI. For thread safety, this method should be invoked from the
-	 * event-dispatching thread.
+	 * Window is closed, signal exit
 	 */
-	private void setupGui() {
-
-		BorderLayout thisLayout = new BorderLayout();
-
-		super.setLayout(thisLayout);
-
-		mainTextArea = new JTextArea();
-		mainTextArea.setOpaque(true);
-		mainTextArea.setBackground(Color.white);
-
-		// Custom component to draw the current image
-		// at a particular offset.
-		setContentPane(mainTextArea);
-
+	public synchronized void windowClosed(WindowEvent evt) {
+		quit = true;
+		this.notifyAll(); // stop all threads
+		System.exit(0);
 	}
 
-	public void start() {
-		super.start();
+	public synchronized void windowClosing(WindowEvent evt) {
+		frame.setVisible(false); // default behaviour of JFrame
+		frame.dispose();
 	}
 
-	public void stop() {
-		super.stop();
+	public synchronized void actionPerformed(ActionEvent evt) {
+		textArea.setText("");
 	}
 
+	/**
+	 * This method is called by new Runnable
+	 */
+	public synchronized void run() {
+		
+		try{
+		    //Loop , but pause to let the user do something!
+			
+			while(this.quit=false){
+				  Thread.sleep(1000);
+
+			}
+			
+ }    catch(java.lang.InterruptedException ex) {}
+
+	
+	}
+
+	public static void main(String[] arg) {
+
+		RulePlayer player = new RulePlayer();
+		
+		//Start a thread to keep window open
+		 Runnable readRun = new Thread(player);
+		 readRun.run();
+	     
+		       
+		
+		player.compileRules();
+		System.out.println("RulePlayer Complete");
+
+
+	}
 }

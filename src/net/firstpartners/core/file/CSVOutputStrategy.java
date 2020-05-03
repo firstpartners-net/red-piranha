5da44f3205b3df3b477158bae31ef0f321ef3c81
paulbrowne <github@firstpartners.net>
2020-05-03 14:26:08 +0100

    Javadoc

--- src/net/firstpartners/core/file/CSVOutputStrategy.java
+++ src/net/firstpartners/core/file/CSVOutputStrategy.java
@@ -21,24 +21,65 @@ import net.firstpartners.data.RangeList;
 /**
  * Strategy class of output of CSV Document.
  * <p>
- * CSV will try to append to an existing CSV file. In General, one document processed equals one line in the CSV file. 
+ * CSV will try to append to an existing CSV file. In general, one document
+ * processed equals one line in the CSV file.
  * 
- * The table below might help understand this Outputters behaviour.
- * <table>
- * <tr><td>name1</td><td>name2</td><td>name3</tr>
- * <tr><td>filea-cell1.value&nbsp;</td><td>filea-cell2.value&nbsp;</td><td>filea-cell3.value&nbsp;</tr>
+ * The table below might help understand this Outputers behaviour.
+ * <table style="border: 1px solid black;">
+ * <tr>
+ * <td>heading-1</td>
+ * <td>heading-2</td>
+ * <td>heading-3</td>
+ * </tr>
+ * <tr>
+ * <td>file-a-cell1.value&nbsp;</td>
+ * <td>file-a-cell2.value&nbsp;</td>
+ * <td>file-a-cell3.value&nbsp;</td>
+ * </tr>
+ * <td>file-b-cell1.value&nbsp;</td>
+ * <td>file-b-cell2.value&nbsp;</td>
+ * <td>file-b-cell3.value&nbsp;</td>
+ * </tr>
  * </table>
- * 
- * <ol>
- * <li>The Outputter looks for a file of the name given, if it exists it will read the headers</li>
- * 
- * Only cells marked with
- * All Output will be on one line in format
- * 
- * 
- * </ol>s
- * 
+ * <p>
+ * The process by which the Outputer works is as follows. It uses the data from
+ * the RangeHolder (our version of the Word / Excel / Other document that was
+ * passed in, that the rules then modified).
+ * </p><p>
+ * Happily, this data is displayed in the Red Piranha GUI - picture below - so
+ * you can see the data you have to work with.
  * </p>
+ * <ol>
+ * <li>The Outputer looks for a csv file of the name given (normally in the
+ * config file); if it does not find it will throw an error.</li>
+ * <li>The Outputer will take the headers (the first line of the CSV file). In
+ * the above example it will be heading-1, heading-2 and heading-3.</li>
+ * <li>For each of these header values, the outputer will search for a @see Cell
+ * of the same cellName from the RangeHolder data.</li>
+ * <li>If it finds a match, it will take the value of that Cell Object and use
+ * it when outputing the line.</li>
+ * <li>After it has tried to find matches, it will then add one line to the CSV
+ * file, using the values found.</li>
+ * </ol>
+ * <p>
+ * While the Outputer will not overwrite any data present, it will not check
+ * for duplicates; running this five times will add five lines of equal data. We
+ * leave it up to your Excel skills to detect duplicates!
+ * </p>
+ * <p>
+ * Some best practices should include
+ * </p>
+ * <ul>
+ * <li>Including a filename or some other unique identifier in the csv output file. That
+ * way you can spot and filter out any duplicate runs.</li>
+ * <li>In your rule file, map your incoming value to an specific outgoing value.
+ * That way your output name should not change if if there are changes to the format
+ * of the source document.</li>
+ * </ul>
+ * </p>
+ * <img src=
+ * "https://paulbrowne-irl.github.io/red-piranha/images/GuiRangeHolder.png" alt="Red Piranha GUI showing RangeHolder Data></img>
+ * 
  * @author paul
  *
  */
@@ -74,7 +115,6 @@ public class CSVOutputStrategy implements IDocumentOutStrategy {
 	 */
 	@Override
 	public void flush(ILogger logger) {
-		
 
 	}
 
@@ -106,9 +146,7 @@ public class CSVOutputStrategy implements IDocumentOutStrategy {
 		Writer writer = new FileWriter(outputFileName, true);
 
 		// write CSV file
-		CSVPrinter printer = CSVFormat.DEFAULT.withHeader("ID", "Name", "Program", "New","University").print(writer);
-		
-		
+		CSVPrinter printer = CSVFormat.DEFAULT.withHeader("ID", "Name", "Program", "New", "University").print(writer);
 
 		// create a list
 		List<Object[]> data = new ArrayList<>();

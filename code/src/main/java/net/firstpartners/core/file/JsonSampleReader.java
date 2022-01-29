6d134b664f2f19400b46162af2182618f774334c
Paul Browne <github@firstpartners.net>
2022-01-29 10:54:50 +0000

    stub JSON reader

---
+++ code/src/main/java/net/firstpartners/core/file/JsonSampleReader.java
@@ -0,0 +1,12 @@
+package net.firstpartners.core.file;
+
+/**
+ * Class to read info on our samples from a file.
+ * It means we can be consistent between the samples we display in the WebApp
+ * And the samples we use in our integration tests
+ * @author paulf
+ *
+ */
+public class JsonSampleReader {
+
+}

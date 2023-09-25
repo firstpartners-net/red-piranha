64377bc8341dff2fdc69f90710d4f62cda54c5c7
Paul Browne <1431190+paulbrowne-irl@users.noreply.github.com>
2023-09-25 15:04:48 +0100

    script outline

---
+++ code/src/test/java/net/firstpartners/core/script/PreProcessorTest.java
@@ -0,0 +1,35 @@
+package net.firstpartners.core.script;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+
+import org.slf4j.LoggerFactory;
+
+import net.firstpartners.core.file.OfficeDocument;
+
+import org.slf4j.Logger;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.junit.Test;
+
+
+public class PreProcessorTest {
+
+	
+	// Handle to the loggers
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	
+	@Test
+	public final void testRunEventRulesExample() {
+		
+		PreProcessor.main(null);
+		
+		
+	}
+
+}
+
